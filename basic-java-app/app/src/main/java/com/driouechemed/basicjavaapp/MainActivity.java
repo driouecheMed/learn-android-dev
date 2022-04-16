85e775a87926215b04b2d5705c39a05e27ded494
driouecheMed <drioueche.med@gmail.com>
2022-04-16 11:46:24 +0000

    Initial Projec: Basic Java App

---
+++ basic-java-app/app/src/main/java/com/driouechemed/basicjavaapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.driouechemed.basicjavaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
