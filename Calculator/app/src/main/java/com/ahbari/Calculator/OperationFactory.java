31e6f8d611e037935f86545e9ce6325d298336ad
Mohammeed AHBARI <84805391+AlmaLmi-byte@users.noreply.github.com>
2022-12-10 11:40:14 +0100

    used factory design pattern

---
+++ Calculator/app/src/main/java/com/ahbari/Calculator/OperationFactory.java
@@ -0,0 +1,34 @@
+package com.ahbari.Calculator;
+import com.ahbari.Calculator.operations.Add;
+import com.ahbari.Calculator.operations.Divide;
+import com.ahbari.Calculator.operations.Modulo;
+import com.ahbari.Calculator.operations.Multiplication;
+import com.ahbari.Calculator.operations.Operation;
+import com.ahbari.Calculator.operations.Subtract;
+
+
+public class OperationFactory {
+    private Operation operation;
+
+    public static double calculate (double a, double b, String op){
+        switch (op){
+            case "+" :
+                return new Add().perform(a,b);
+
+            case "-" :
+                return new Subtract().perform(a,b);
+
+            case "*" :
+                return new Multiplication().perform(a,b);
+
+            case "/" :
+                return new Divide().perform(a,b);
+
+            case "%" :
+                return new Modulo().perform(a,b);
+            default:
+                return b;
+        }
+    }
+
+}
