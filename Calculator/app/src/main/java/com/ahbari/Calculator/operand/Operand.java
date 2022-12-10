0c1fc0d3c533b6260261c31cd7a4c39a8bdcc608
Mohammeed AHBARI <84805391+AlmaLmi-byte@users.noreply.github.com>
2022-12-10 11:43:00 +0100

    Operand an Modulo Classes

---
+++ Calculator/app/src/main/java/com/ahbari/Calculator/operand/Operand.java
@@ -0,0 +1,45 @@
+package com.ahbari.Calculator.operand;
+
+public class Operand {
+    private String operand ;
+
+    public Operand(){
+        this.operand = "0";
+    }
+
+    public void addDigit(String d) {
+        if (d.matches("^[0-9]+$")){
+            if (this.operand.equals("0")){
+                this.operand = d;
+            } else {
+                this.operand += d;
+            }
+        } else if (d.equals(".")) {
+            if(!this.operand.contains(".")){
+                this.operand += d;
+            }
+        }
+    }
+
+    public void back(){
+        if (this.operand.length() <= 1){
+            this.operand = "0" ;
+        } else {
+            this.operand = this.operand.substring(0,  this.operand.length()-1);
+        }
+    }
+
+    public void clear() { this.operand = "0"; }
+
+    public String getOperand (){ return this.operand; }
+
+    public boolean isEmpty() { return this.operand.equals("0"); }
+
+    public double pop(){
+        double res = Double.parseDouble(operand);
+        clear();
+        return res;
+    }
+
+
+}
