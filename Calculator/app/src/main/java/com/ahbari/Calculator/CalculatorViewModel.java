5b8085fc924debfd394ae0dfb9612f2820f96b09
Mohammeed AHBARI <84805391+AlmaLmi-byte@users.noreply.github.com>
2022-12-10 11:41:35 +0100

    fixing the Calculator view model

--- Calculator/app/src/main/java/com/ahbari/Calculator/CalculatorViewModel.java
+++ Calculator/app/src/main/java/com/ahbari/Calculator/CalculatorViewModel.java
@@ -7,27 +7,21 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import com.ahbari.Calculator.operations.Add;
-import com.ahbari.Calculator.operations.Divide;
-import com.ahbari.Calculator.operations.Modelo;
-import com.ahbari.Calculator.operations.Multiplication;
-import com.ahbari.Calculator.operations.Operation;
-import com.ahbari.Calculator.operations.Subtract;
+import com.ahbari.Calculator.operand.Operand;
 
 public class CalculatorViewModel extends AndroidViewModel {
 
     private final MutableLiveData<String> numberToDisplay = new MutableLiveData<>();
     private final MutableLiveData<String> expressionToDisplay = new MutableLiveData<>();
-    Calculator calculator;
-    OperandAdapter operandAdapter;
-    double currentValue ;
-    String currentOperation ;
+    private Operand operand;
+    private double currentValue ;
+    private String currentOperation ;
 
     public CalculatorViewModel(@NonNull Application application) {
         super(application);
-        calculator = new Calculator();
-        operandAdapter = new OperandAdapter();
+        operand = new Operand();
         currentValue = 0.0;
+        currentOperation = "";
     }
 
     public LiveData<String> getNumberToDisplay() {
@@ -38,26 +32,24 @@ public class CalculatorViewModel extends AndroidViewModel {
     }
 
     public void back() {
-        operandAdapter.back();
+        operand.back();
     }
 
     public void clearAll() {
-        operandAdapter.clear();
+        operand.clear();
         this.numberToDisplay.setValue("0");
         this.expressionToDisplay.setValue("");
     }
 
     public void clearInput() {
-        operandAdapter.clear();
+        operand.clear();
         this.numberToDisplay.setValue("0");
     }
 
     public void equal() {
-        if (operandAdapter.isEmpty()){
-            return;
-        }
+        if (operand.isEmpty()){ return; }
 
-        double newNumber = operandAdapter.pop();
+        double newNumber = operand.pop();
         if (currentValue != 0.0) {
             String expression = currentValue + currentOperation + newNumber + " =" ;
             expressionToDisplay.setValue(expression);
@@ -65,50 +57,25 @@ public class CalculatorViewModel extends AndroidViewModel {
             expressionToDisplay.setValue(String.valueOf(newNumber));
         }
 
-        this.currentValue = calculator.calculate(currentValue, newNumber);
+        this.currentValue = OperationFactory.calculate(currentValue, newNumber, currentOperation);
         this.numberToDisplay.setValue(String.valueOf(currentValue));
-
     }
+
     public void setOperation(String op) {
-        if (op.equals(currentOperation)){
-            return;
-        }
+        if (op.equals(currentOperation)){ return; }
 
-        this.currentOperation = op;
-        switch (op){
-            case "+" :
-                handelOperation(new Add());
-                break;
-            case "-" :
-                handelOperation(new Subtract());
-                break;
-            case "*" :
-                handelOperation(new Multiplication());
-                break;
-            case "/" :
-                handelOperation(new Divide());
-                break;
-            case "%" :
-                handelOperation(new Modelo());
-                break;
-        }
-    }
-
-    void handelOperation(Operation op) {
-        if (operandAdapter.isEmpty()){
-            calculator.setOperation(op);
-        } else {
-            double newNumber = operandAdapter.pop();
-            currentValue = calculator.calculate(currentValue , newNumber);
-            calculator.setOperation(op);
+        if (!operand.isEmpty()){
+            double newNumber = operand.pop();
+            currentValue = OperationFactory.calculate(currentValue , newNumber, currentOperation);
             numberToDisplay.setValue(String.valueOf(currentValue));
         }
+        this.currentOperation = op;
         expressionToDisplay.setValue(currentValue + " " + currentOperation);
     }
 
     public void addDigit(String d) {
-        operandAdapter.addDigit(d);
-        numberToDisplay.setValue(operandAdapter.getOperand());
+        operand.addDigit(d);
+        numberToDisplay.setValue(operand.getOperand());
     }
 
 }
