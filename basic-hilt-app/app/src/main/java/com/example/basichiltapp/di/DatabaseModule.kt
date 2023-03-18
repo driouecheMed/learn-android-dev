8566cb024fffdb275b11008e53efe36b830f80cb
driouecheMed <drioueche.med@gmail.com>
2023-03-18 17:26:37 +0100

    Add database

---
+++ basic-hilt-app/app/src/main/java/com/example/basichiltapp/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.example.basichiltapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.basichiltapp.database.AppDatabase
+import com.example.basichiltapp.database.daos.UserDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        val databaseName = "learn_hilt"
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            databaseName
+        ).build()
+    }
+
+    @Provides
+    fun provideUserDao(appDatabase: AppDatabase): UserDao {
+        return appDatabase.UserDao()
+    }
+}
